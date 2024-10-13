07a5e4ce218acbbef9ee22459b719e66da334410
swwho96 <swwho96@gmail.com>
2024-10-13 23:19:25 +0900

    add: chat templete 생성
    openai를 활용한 챗봇을 테스트 하기 위한 템플릿 생성

---
+++ Chatbot.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import streamlit as st
+
+with st.sidebar:
+    password = st.text_input("Password", key="chatbot_api_key", type="password")
+
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not password or password != st.secrets["Password"]:
+        st.info("정확한 API Key를 입력해주세요.")
+        st.stop()
+
+    client = OpenAI(api_key=st.secrets['OpenAI_Key'])
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
