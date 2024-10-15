4d61e878cc7602e632d094adf4f6627ef7f2b9cb
swwho96 <swwho96@gmail.com>
2024-10-15 20:46:39 +0900

    update: 화면 구성 완료
    vision 없는 1차 화면 구성

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 from collections import deque
 import streamlit as st
+import base64
 from utils.prompts import CHAT_PROMPT, QUIZ_PROMPT
 
 st.set_page_config(page_title="너나들이", page_icon="https://avatars.githubusercontent.com/u/179866435?s=48&v=5")
@@ -13,13 +14,11 @@ with st.sidebar:
         st.session_state['system_prompt'] = [{"role":"assistant", "content":CHAT_PROMPT}]
     elif type == 'Quiz':
         st.session_state['system_prompt'] = [{"role":"assistant", "content":QUIZ_PROMPT}]
-    elif type == 'Image':
-        file = st.file_uploader(label='이미지를 업로드 하세요', type=['jpg', 'png', 'jpeg'])
-        st.session_state['system_prompt'] = [{"role":"assistant", "content":QUIZ_PROMPT}]
     st.divider()
     "[![QA 제보](https://avatars.githubusercontent.com/u/179866435?s=48&v=)](https://www.notion.so/9bfd00945c0f41c285fd165f4810ff75?pvs=4 'QA 페이지')"
 
 st.title(f"{type}")
+
 
 
 if "messages" not in st.session_state:
@@ -33,6 +32,8 @@ if prompt := st.chat_input():
         st.info("Password를 확인해주세요.")
         st.stop()
     client = OpenAI(api_key=st.secrets['OpenAI_Key'])
+    # if type == "Image":
+    #     st.session_state.messages.append({"role": "user", "content": [{"type":"text", "text":prompt}, {"type":"image_url", "image_url": {"url": f"data:image/png;base64,{encoded_img}"}}]})
     st.session_state.messages.append({"role": "user", "content": prompt})
     
     # message 수 9개 이하로 조정 
@@ -41,7 +42,10 @@ if prompt := st.chat_input():
         st.session_state.messages.popleft()
 
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-4o-2024-08-06", messages=st.session_state.system_prompt+list(st.session_state.messages))
+    response = client.chat.completions.create(
+        model="gpt-4o-mini", 
+        messages=st.session_state.system_prompt+list(st.session_state.messages)
+    )
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
