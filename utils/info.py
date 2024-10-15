5cf5d9e557c112a5aef845d812c5ddfafc1406f1
swwho96 <swwho96@gmail.com>
2024-10-16 00:47:52 +0900

    add: 안내사항 추가
    테스트를 및 앱 사용을 위한 안내사항 업데이트

---
+++ utils/info.py
@@ -0,0 +1,64 @@
+INFO = """
+
+<style>
+@font-face {
+    font-family: 'NanumSquareNeo';
+    src: url(https://hangeul.pstatic.net/hangeul_static/webfont/NanumSquareNeo/NanumSquareNeoTTF-bRg.eot);
+    src: url(https://hangeul.pstatic.net/hangeul_static/webfont/NanumSquareNeo/NanumSquareNeoTTF-bRg.eot?#iefix) format("embedded-opentype"), url(https://hangeul.pstatic.net/hangeul_static/webfont/NanumSquareNeo/NanumSquareNeoTTF-bRg.woff) format("woff"), url(https://hangeul.pstatic.net/hangeul_static/webfont/NanumSquareNeo/NanumSquareNeoTTF-bRg.ttf) format("truetype");
+}
+.custom-font {
+    font-family: 'NanumSquareNeo', sans-serif;
+    font-size: 24px;
+}
+</style>
+
+
+<div align="center" class='custom-font'> 
+
+<h1>너나들이 챗봇 테스트 안내문</h1>
+
+
+<img src="https://github.com/user-attachments/assets/3884c8e5-69b9-4835-ba99-7e285d7bb579" 
+    style="height: 200px; width: 90%; 
+            object-fit: cover; 
+            border-radius: 20px;" >
+
+  
+안녕하세요! 챗봇 테스트 앱입니다.
+
+여러분은 너나들이 서비스에 들어갈 챗봇을 테스트 하실 예정입니다.
+
+다음 안내사항을 잘 읽고 테스트를 진행해주세요.  
+
+</div>
+<div align=center class='custom-font'>
+
+<hr style="margin: 20px;">
+
+# 기본대화  
+
+기본 대화는 국가 유산에 관해 자유롭게 대화를 나눌 수 있는 기능입니다.  
+
+국가 유산에 관해서만 답변을 하는지 확인해주세요  
+
+너무 길거나 짧은 답변을 하지 않는지 확인해주세요  
+
+부정확한 답변이라 생각된다면, 검색을 통해 확인 후 알려주세요  
+
+<hr style="margin: 20px;">
+
+# 퀴즈
+
+퀴즈는 국가 유산에 관한 퀴즈를 정해진 형식에 맞게 출제하는 기능입니다.  
+
+추후 퀴즈를 풀고 정답을 맞추는 형식을 갖출 예정입니다.  
+
+국가 유산에 관해서만 퀴즈를 출제하는지 확인해주세요  
+
+정해진 형식에 맞게 출제하는지 확인해주세요 (문제, 5지선다(한줄에 하나씩), 정답, 설명 순)  
+
+선지가 너무 길거나, 정답이 맞지 않거나, 설명이 부정확한지 확인해주세요  
+
+</div>
+
+"""
