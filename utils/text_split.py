4a12d01fdd926e703da001b5237436872755eda6
swwho96 <swwho96@gmail.com>
2024-10-29 21:03:49 +0900

    add: 텍스트 형식 관리 함수 추가
    LLM 반환 형식을 각 기능에 맞게 수정하는 함수를 담음 파일 생성

---
+++ utils/text_split.py
@@ -0,0 +1,39 @@
+def quiz():
+    return
+
+def summary(texts:list):
+    start_date = texts[0]
+    end_date = texts[1]
+    completions = texts[2].split('\n')[0]
+    tags = texts[2].split('\n')[-1]
+    text = f"""
+    <style>
+    @font-face {{
+        font-family: 'NanumSquareNeo';
+        src: url(https://hangeul.pstatic.net/hangeul_static/webfont/NanumSquareNeo/NanumSquareNeoTTF-bRg.eot);
+        src: url(https://hangeul.pstatic.net/hangeul_static/webfont/NanumSquareNeo/NanumSquareNeoTTF-bRg.eot?#iefix) format("embedded-opentype"), url(https://hangeul.pstatic.net/hangeul_static/webfont/NanumSquareNeo/NanumSquareNeoTTF-bRg.woff) format("woff"), url(https://hangeul.pstatic.net/hangeul_static/webfont/NanumSquareNeo/NanumSquareNeoTTF-bRg.ttf) format("truetype");
+    }}
+    .custom-font {{
+        font-family: 'NanumSquareNeo', sans-serif;
+        font-size: 24px;
+    }}
+    </style>
+
+    <div class='custom-font'> 
+
+    <h1 align='center'>대화 요약</h1>
+
+    채팅 시작일: {start_date}  
+
+    채팅 종료일: {end_date}  
+
+    <hr style="margin: 20px;">
+
+    {completions}
+
+    <hr style="margin: 20px;">
+
+    <strong>{tags}</strong>
+    </div>
+    """
+    return text
