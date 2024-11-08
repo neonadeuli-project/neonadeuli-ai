814ece420c234120907c0ac19cf16725e741d2ba
swwho96 <swwho96@gmail.com>
2024-11-08 17:38:02 +0900

    update: 챗봇 세분화 및 퀴즈 챗봇 별도 구성
    - 기존 챗봇에 '국가유산해설사' 챗봇 추가
    - 퀴즈 챗봇은 별도의 url로 마련
    - 모델을 4o로 변경

--- Chatbot.py
+++ Chatbot.py
@@ -1,18 +1,21 @@
 from openai import OpenAI
 from collections import deque
 import streamlit as st
-from utils.prompts import CHAT_PROMPT, QUIZ_PROMPT, SUMMARY_PROMPT
+from utils.prompts import CHAT_PROMPT, SUMMARY_PROMPT, COMMENTATOR_PROMPT
 from utils.info import INFO
 from utils.text_split import quiz, summary
 from datetime import datetime
 import time
 
+# 파비콘
 st.set_page_config(page_title="너나들이", page_icon="https://avatars.githubusercontent.com/u/179866435?s=48&v=5")
 
+# 안내사항
 @st.dialog(title=" ", width="large")
 def info():
     st.markdown(INFO, unsafe_allow_html=True)
 
+# 대화 요약 버튼
 @st.dialog(title=" ", width="large")
 def summarize():
     client = OpenAI(api_key=st.secrets['OpenAI_Key'])
@@ -24,22 +27,33 @@ def summarize():
         )
     st.markdown(summary(["2024-07-10", datetime.today().strftime("%Y-%m-%d"), summarized_content.choices[0].message.content]), unsafe_allow_html=True)
 
+# 사이드바 구성
 with st.sidebar:
     password = st.text_input("Password", key="chatbot_api_key", type="password")
     st.divider()
-    type = st.radio("챗봇 유형을 선택해주세요.",["Chatbot", "Quiz"], captions=["국가 유산에 관해 대화하는 챗봇", "국가 유산에 관한 퀴즈를 출제하는 챗봇"])
-    if type == 'Chatbot':
-        st.session_state['system_prompt'] = [{"role":"assistant", "content":CHAT_PROMPT}]
-    elif type == 'Quiz':
-        st.session_state['system_prompt'] = [{"role":"assistant", "content":QUIZ_PROMPT}]
+    chatbot_type = st.radio(
+        "챗봇의 유형을 선택하세요",
+        ["국가유산챗봇", "국가유산해설사"],
+        captions=["국가유산과 채팅을 주고받습니다.", "국가유산해설사와 같은 역할을 수행합니다."]
+    )
+    if "chatbot_type" in st.session_state and chatbot_type != st.session_state['chatbot_type']:
+        st.session_state["messages"] = deque([])
+    st.session_state['chatbot_type'] = chatbot_type
     st.divider()
+    if chatbot_type == "국가유산해설사":
+        option = st.selectbox(
+            label="국가유산을 선택하세요",
+            options=("경복궁", "불국사")
+        )
+        st.divider()
+    st.session_state['system_prompt'] = [{"role":"system","content":CHAT_PROMPT}] if chatbot_type == "국가유산챗봇" else [{"role":"system","content":COMMENTATOR_PROMPT(option)}]
     if st.button("안내사항", icon="⚠️"): info()
     st.link_button(label="너나들이", url="https://www.notion.so/9bfd00945c0f41c285fd165f4810ff75?pvs=4", help="너나들이 QA페이지", icon="🚨")
     if "messages" in st.session_state and len(st.session_state.messages) >= 2:
         if st.button("대화요약", icon="📝"): summarize()
 
-st.title(f"{type}")
-
+# 중앙 타이틀
+st.title(f"{chatbot_type}" if chatbot_type == "국가유산챗봇" else f"{chatbot_type} - {option}")
 
 
 if "messages" not in st.session_state:
@@ -48,14 +62,11 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-
 if input := st.chat_input():
     if not password or password != st.secrets["Password"]:
         st.info("Password를 확인해주세요.")
         st.stop()
     client = OpenAI(api_key=st.secrets['OpenAI_Key'])
-    # if type == "Image":
-    #     st.session_state.messages.append({"role": "user", "content": [{"type":"text", "text":prompt}, {"type":"image_url", "image_url": {"url": f"data:image/png;base64,{encoded_img}"}}]})
     st.session_state.messages.append({"role": "user", "content": input})
     
     # message 수 9개 이하로 조정 
@@ -65,7 +76,7 @@ if input := st.chat_input():
 
     st.chat_message("user").write(input)
     response = client.chat.completions.create(
-        model="gpt-4o-mini", 
+        model="gpt-4o", 
         messages=st.session_state.system_prompt+list(st.session_state.messages)
     )
     msg = response.choices[0].message.content
